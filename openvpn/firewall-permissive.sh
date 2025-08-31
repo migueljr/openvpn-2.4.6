e1aa73e389ed893d076e3d58921cf94ab1c8151a
migueljr <miguel_jr@mail.com>
2025-08-31 19:52:40 -0300

    ajustes

--- openvpn/firewall-permissive.sh
+++ openvpn/firewall-permissive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-# This iptables script to controlling traffic in the openvpn tunnel.
-# In this example, clients can only perform DNS, HTTP and HTTPS requests to the world.
+# This iptables script allows more permissive traffic in the openvpn tunnel.
+# Allows HTTP traffic on any port and common application ports.
 
 # Check if iptables is available and working
 if ! command -v iptables >/dev/null 2>&1; then
@@ -19,9 +19,15 @@ fi
 
 # Drop everything by default from tunnel to world
 iptables -P FORWARD DROP
+
 # Allow DNS from tunnel to world
 iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+
 # Allow HTTP and HTTPS from tunnel to world
 iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p tcp -m tcp -m conntrack --ctstate NEW -m multiport --dports 80,443 -j ACCEPT
-# Allow port 5001 for the specific application
-iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p tcp -m tcp --dport 5001 -m conntrack --ctstate NEW -j ACCEPT
+
+# Allow common application ports
+iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p tcp -m tcp -m conntrack --ctstate NEW -m multiport --dports 5001,8080,3000,8000,9000 -j ACCEPT
+
+# Allow established connections
+iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
