3fa7c66a3597d214cbdd17cba3d99d38c9a95f9d
migueljr <miguel_jr@mail.com>
2025-08-31 20:06:36 -0300

    fix: ajustes

--- openvpn/firewall.sh
+++ openvpn/firewall.sh
@@ -23,5 +23,3 @@ iptables -P FORWARD DROP
 iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 # Allow HTTP and HTTPS from tunnel to world
 iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p tcp -m tcp -m conntrack --ctstate NEW -m multiport --dports 80,443 -j ACCEPT
-# Allow port 5001 for the specific application
-iptables -A FORWARD -i tun+ -o "$NAT_INTERFACE" -p tcp -m tcp --dport 5001 -m conntrack --ctstate NEW -j ACCEPT
